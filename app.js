e02282506d9567d7c678662ca4ef1b5275909dbf
ikenna <mrteks2022@gmail.com>
2024-03-23 12:34:15 -0700

    fronted

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Define API endpoint URLs
-const baseUrl = 'http://localhost:3000/api/inventory';
+//const baseUrl = 'http://localhost:3000/api/inventory';
+const baseUrl = 'https://eventry-backend-8.onrender.com';
 
 // Function to fetch and display inventory items
 function fetchInventoryItems() {
